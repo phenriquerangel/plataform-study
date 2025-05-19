c2b75531a4d55b1cf4e3b9485a0e2f57ac85a731
Pedro Henrique Rangel Junger de Assis <phenriquerangel@gmail.com>
2025-05-19 16:06:58 -0300

    aaa

--- frontend/src/pages/Monitoring.tsx
+++ frontend/src/pages/Monitoring.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API = import.meta.env.VITE_API_URL || "http://backend.backend.svc.cluster.local:8000";
 
 export default function Monitoring() {
   const [status, setStatus] = useState<"ok" | "erro" | "loading">("loading");
